ef4c2a854a83ac86210633fe94654c4d0dc6fe06
SiriusBlue <61279808+SiriusBlue7@users.noreply.github.com>
2021-05-15 12:32:24 +0200

    Comentado
    He comentado todas las funciones que había con comentarios de lo que hacen en general y de lo que hace cada parte.
    Puede que haya que modificarlo luego para el tema de las descripciones

--- WEB-INF/src/twitter/DBManager.java
+++ WEB-INF/src/twitter/DBManager.java
@@ -42,18 +42,26 @@ public class DBManager implements AutoCloseable {
         }
         connection = null;
     }
-
+    /*
+    *
+    *Buscamos el usuario por medio de su short_name
+    *
+    */
     public User searchUser(String short_name) throws SQLException {
       // crea la busqueda
       String query = "SELECT * FROM Usuarios WHERE short_name=?";
       // creamos el usuario para rellenarlo
       User usuario = null;
+      //creamos el usuario a null, para que no haya que hacer cambios
       try ( PreparedStatement ps = connection.prepareStatement(query)) {
-        ps.setString(1, short_name);
-        ResultSet rs = ps.executeQuery();
-        // si el resultado existe lo pasamos al usuario creado
-        if(rs.next()){
 
+        ps.setString(1, short_name);//rellenamos el statement con el String de forma segura
+
+        ResultSet rs = ps.executeQuery();
+        //ejecutamos la consulta en la BD
+        if(rs.next()){
+          //SI la consulta fue existosa, nos devuelve una línea
+          //Recorremos las columnas de la consulta almacenando los datos en el User para devolverlo
           usuario = new User();
           usuario.setId(rs.getInt("id"));
           usuario.setShort_name(rs.getString("short_name"));
@@ -64,79 +72,11 @@ public class DBManager implements AutoCloseable {
       }
       return usuario;
     }
-
-    //
-    public User authenticate(String short_name, String password) throws SQLException {
-      // crea la busqueda
-      String query = " SELECT * FROM Usuarios WHERE short_name=? AND password=?";
-      // creamos el usuario para rellenarlo
-      User usuario = null;
-      try ( PreparedStatement ps = connection.prepareStatement(query)) {
-        ps.setString(1, short_name);
-        ps.setString(2,password);
-        ResultSet rs = ps.executeQuery();
-        // si el resultado existe lo pasamos al usuario creado
-        if(rs.next()){
-          usuario = new User();
-          usuario.setId(rs.getInt("id"));
-          usuario.setShort_name(rs.getString("short_name"));
-          usuario.setLong_name(rs.getString("long_name"));
-          usuario.setMail(rs.getString("mail"));
-          usuario.setPassword(rs.getString("password"));
-        }
-      }
-        return usuario;
-    }
-
     /*
-     * esta funcion comprueba si el nombre, el nombre corto y el email
-     * estan libres en la base de datos
-     * return:
-     * 1: el nombre corto ya esta registrado
-     * 2: el nombre ya esta registrado
-     * 3: el mail ya esta registrado
-     * 0: ninguno de los parametros esta registrado
-     */
-    public int isDataFree(String name, String shortName, String email) throws SQLException{
-      String query_short = " SELECT short_name FROM Usuarios WHERE short_name=?";
-      String query_name = " SELECT long_name FROM Usuarios WHERE long_name=?";
-      String query_mail = " SELECT mail FROM Usuarios WHERE mail=?";
-      try ( PreparedStatement ps = connection.prepareStatement(query_short);
-            PreparedStatement pn = connection.prepareStatement(query_name);
-            PreparedStatement pm = connection.prepareStatement(query_mail)) {
-        ps.setString(1, shortName);
-        pn.setString(1, name);
-        pm.setString(1, email);
-
-        ResultSet rs = ps.executeQuery();
-        ResultSet rn = pn.executeQuery();
-        ResultSet rm = pm.executeQuery();
-
-        if(rs.next()) return 1;
-        if(rn.next()) return 2;
-        if(rm.next()) return 3;
-        return 0;
-      }
-    }
-
-    /*
-     * añade un usuario a la base de datos
-     *
-     */
-    public void addUser(User usuario) throws SQLException{
-
-      String query = "INSERT INTO Usuarios (short_name, long_name, mail, password) VALUES (?, ?,?,?)";
-
-      try ( PreparedStatement ps = connection.prepareStatement(query)){
-        ps.setString(1, usuario.getShort_name());
-        ps.setString(2, usuario.getLong_name());
-        ps.setString(3, usuario.getMail());
-        ps.setString(4, usuario.getPassword());
-
-        int rowcount = ps.executeUpdate();
-      }
-    }
-
+    *
+    *Buscamos el usuario por de su id
+    *
+    */
     public User searchUser(int id) throws SQLException{
         String query = "SELECT * FROM Usuarios WHERE id=?";
         User user = null;
@@ -158,11 +98,91 @@ public class DBManager implements AutoCloseable {
       return user;
   	}
 
+    //Usamos esta funciona para comprobar si lois datos introducidos en el login coinciden con los de algun usuario almacenado
+    public User authenticate(String short_name, String password) throws SQLException {
+      // crea la busqueda
+      String query = " SELECT * FROM Usuarios WHERE short_name=? AND password=?";//Vemos si hay un usuario con el shortname y la contraseña
+      // creamos el usuario para rellenarlo
+      User usuario = null;
+      try ( PreparedStatement ps = connection.prepareStatement(query)) {
+        //rellenamos la consulta con los datos, de forma segura con el Prepared Statement
+        ps.setString(1, short_name);
+        ps.setString(2,password);
+        //Ejecutamos la consulta
+        ResultSet rs = ps.executeQuery();
+        // si el resultado existe lo pasamos al usuario creado
+        if(rs.next()){
+          //Si la consulta tiene una línea, es que ha sido exitosa, por lo que rellenamos el user, para almacenarlo en la sesion
+          usuario = new User();
+          usuario.setId(rs.getInt("id"));
+          usuario.setShort_name(rs.getString("short_name"));
+          usuario.setLong_name(rs.getString("long_name"));
+          usuario.setMail(rs.getString("mail"));
+          usuario.setPassword(rs.getString("password"));
+        }
+      }
+        return usuario;
+    }
+
+    /*
+     * esta funcion comprueba si el nombre, el nombre corto y el email
+     * estan libres en la base de datos
+     * return:
+     * 1: el nombre corto ya esta registrado
+     * 2: el nombre ya esta registrado
+     * 3: el mail ya esta registrado
+     * 0: ninguno de los parametros esta registrado
+     */
+    public int isDataFree(String name, String shortName, String email) throws SQLException{
+      //Preparamos las distintas consultas para comprobar si los campos a rellenar estan libres
+      String query_short = " SELECT short_name FROM Usuarios WHERE short_name=?";
+      String query_name = " SELECT long_name FROM Usuarios WHERE long_name=?";
+      String query_mail = " SELECT mail FROM Usuarios WHERE mail=?";
+
+      try ( PreparedStatement ps = connection.prepareStatement(query_short);
+            PreparedStatement pn = connection.prepareStatement(query_name);
+            PreparedStatement pm = connection.prepareStatement(query_mail)) {
+        //rellenamos los statement con los datos introducidos por el usuario
+        ps.setString(1, shortName);
+        pn.setString(1, name);
+        pm.setString(1, email);
+
+        ResultSet rs = ps.executeQuery();
+        ResultSet rn = pn.executeQuery();
+        ResultSet rm = pm.executeQuery();
+        //Comprobamos los resultados de las consultas, si ninguna nos devuelve una línea, significa que la informacion esta libre
+        if(rs.next()) return 1;
+        if(rn.next()) return 2;
+        if(rm.next()) return 3;
+        return 0;
+      }
+    }
+
+    /*
+     * añade un usuario a la base de datos
+     *
+     */
+    public void addUser(User usuario) throws SQLException{
+      //Creamos la onsulta, con todos los datos a rellenar en la BD
+      //No indicamos el id, ya que al ser autoincremental , se rellena solo de esta forma y asi evitamos problemas
+      String query = "INSERT INTO Usuarios (short_name, long_name, mail, password) VALUES (?, ?,?,?)";
+
+      try ( PreparedStatement ps = connection.prepareStatement(query)){
+        //Rellenamos todos los campos de la consulta de forma segura
+        ps.setString(1, usuario.getShort_name());
+        ps.setString(2, usuario.getLong_name());
+        ps.setString(3, usuario.getMail());
+        ps.setString(4, usuario.getPassword());
+
+       ps.executeUpdate();//Ejecutamos el update para subir los archivos
+      }
+    }
+
+
     /*
      * devuelve un ArrayList con los mensajes relacionados con el id
      * del usuario
      */
-     //Necesaria revision y terminar
     public List<Message> listMessages(int id) throws SQLException{
       String query = "SELECT Mensajes.id AS idmensaje , Mensajes.text AS mensajes , Mensajes.userId AS iduser ,  Mensajes.respuesta AS respuesta , Mensajes.retweet AS retweet , Mensajes.fecha AS fecha FROM Usuarios INNER JOIN Seguidos ON Usuarios.id = Seguidos.seguido INNER JOIN Mensajes ON Usuarios.id = Mensajes.userId WHERE Seguidos.user = ? ORDER BY Mensajes.fecha DESC LIMIT 50";
 
@@ -201,10 +221,15 @@ public class DBManager implements AutoCloseable {
       return buzon;
     }
 
+    /*
+    *
+    *En esta funcion lo que hacemos es buscar todos los mensajes que ha escrito el usuario para imprimirlos en su perfil
+    *Es parecido al listMessages pero solo buscando los mensajes  con el idUser del usuario
+    *Devuelve una lista de como mucho 50 mensajes
+    */
     public List<Message> listUserMessage(int id) throws SQLException{
       String query="SELECT * FROM Mensajes WHERE userId=? ORDER BY fecha DESC LIMIT 50";
-      ArrayList<Message> buzon = new ArrayList<Message>();
-
+      ArrayList<Message> buzon = new ArrayList<Message>();//Creamos el ArrayList en el que vamos a ir añadiendo los mensajes
       try(PreparedStatement stmt = connection.prepareStatement(query)) {
 
         stmt.setInt(1, id);
@@ -214,7 +239,7 @@ public class DBManager implements AutoCloseable {
 
 		      Message mensaje = new Message();
           int retweet = rs.getInt("retweet");
-          if (retweet<=0) {
+          if (retweet<=0) {//Si el campo retweet es = que 0 , es como si estuviese a null, por lo que no habria que hacer nada raro
             //si el mensaje tiene el valor de retweet igual a null, entonces es un mensaje normal
             mensaje.setRetweet(retweet);
             mensaje.setId(rs.getInt("id"));
@@ -225,7 +250,6 @@ public class DBManager implements AutoCloseable {
             User usuario = searchUser(user_id);
             mensaje.setShortName(usuario.getShort_name());
             mensaje.setLongName(usuario.getLong_name());
-
 
           }else{//en el caso de que el campo sea distitnto de null, tenemos que buscar el mensaje original
                 //y rellenar los campos con los datos de ese mensaje
@@ -238,12 +262,20 @@ public class DBManager implements AutoCloseable {
       return buzon;
     }
 
+    /*
+    *
+    *Busca un mensaje por su id
+    *
+    */
     public Message searchMessage(int id) throws SQLException{
-      String query = " SELECT * FROM Mensajes WHERE id=? ORDER BY Mensajes.fecha DESC";
+      //preparamos la consulta de la búsqueda
+      String query = " SELECT * FROM Mensajes WHERE id=?";//buscamos un mensaje cuyo id sea el deseado
       // creamos el usuario para rellenarlo
       Message mensaje = null;
       try ( PreparedStatement ps = connection.prepareStatement(query)) {
+        //rellenamos el campo con el dato introducido
         ps.setInt(1, id);
+        //ejecutamos la consulta
         ResultSet rs = ps.executeQuery();
         // si el resultado existe lo pasamos al usuario creado
         if(rs.next()){
@@ -265,6 +297,12 @@ public class DBManager implements AutoCloseable {
       return mensaje;
     }
 
+    /*
+    *
+    *Busca todos los mensajes que responden a un mensaje con X id
+    *Se hace una consulta que devuelve todos los mensajes que tienen en la colummna respuesta ese id
+    *
+    */
     public List<Message> listAnswer(int id) throws SQLException{
       String query = " SELECT * FROM Mensajes WHERE respuesta=?";
 
@@ -279,7 +317,7 @@ public class DBManager implements AutoCloseable {
 
 		      Message mensaje = new Message();
 
-          mensaje.setId(id);
+          mensaje.setId(rs.getInt("id"));
           mensaje.setText(rs.getString("texto"));
           mensaje.setDate(rs.getTimestamp("fecha"));
           mensaje.setRetweet(rs.getInt("retweet"));
@@ -289,17 +327,23 @@ public class DBManager implements AutoCloseable {
           mensaje.setShortName(usuario.getShort_name());
           mensaje.setLongName(usuario.getLong_name());
 
-			    buzon.add(mensaje);
+			    buzon.add(mensaje);//añadimos el mensaje a la List
         }
       }
-      return buzon;
-
+      return buzon;//Devolvemos el buzon
     }
 
+    /*
+    *
+    *Creamos el mensaje con los datos que se nos pasan y los añadimos a la BD
+    *
+    */
     public void addMessage(Message mensaje) throws SQLException{
+      //Creamos la query, en la que indicamos
       String query = "INSERT INTO Mensajes (userId  , text , fecha) VALUES (? , ? , ?)";
 
       try ( PreparedStatement ps = connection.prepareStatement(query)){
+        //Rellenamos los valores del PreparedStatement de forma segura
         ps.setInt(1, mensaje.getUserId());
         ps.setString(2, mensaje.getText());
         //Cogemos la hora en el servidor
@@ -307,7 +351,7 @@ public class DBManager implements AutoCloseable {
         Timestamp ts=new Timestamp(date.getTime());//Cambiamos el tipo de la hora para poder añadirlo con Timestamp
         ps.setTimestamp(3,ts);
 
-        ps.executeUpdate();
+        ps.executeUpdate();//Ejecutamos el update
       }
     }
 
@@ -315,15 +359,16 @@ public class DBManager implements AutoCloseable {
       //Introducmos en la funcioin el mensaje que queremos crear y el id del mensaje al que estamos respondiendo
       String query = "INSERT INTO Mensajes (userId ,respuesta , text , fecha) VALUES (? , ? , ? , ?)";
       try ( PreparedStatement ps = connection.prepareStatement(query)){
+        //Rellenamos los datos del prepareStatement de forma segura
         ps.setInt(1, mensaje.getUserId());
-        ps.setString(2, mensaje.getText());
-        ps.setInt(3,id);
+        ps.setInt(2,id);
+        ps.setString(3, mensaje.getText());
         //Cogemos la hora en el servidor
         Date date = new Date();
         Timestamp ts=new Timestamp(date.getTime());//Cambiamos el tipo de la hora para poder añadirlo con Timestamp
         ps.setTimestamp(4,ts);
 
-        ps.executeUpdate();
+        ps.executeUpdate();//Ejecutamos el update
       }
     }
 
@@ -339,28 +384,44 @@ public class DBManager implements AutoCloseable {
         Timestamp ts=new Timestamp(date.getTime());//Cambiamos el tipo de la hora para poder añadirlo con Timestamp
         ps.setTimestamp(3,ts);
 
-        ps.executeUpdate();
+        ps.executeUpdate();//Ejecutamos el update
       }
     }
 
+    /*
+    *El usuario con id1 le da a aseguir al usuario id2 y actualizamos la BD para que se añada el elemento
+    */
     public void follow(int id1, int id2) throws SQLException{
+      //Creamos el query con el que insertamos el nuevo elemento en la tabla de seguidos
       String query = "INSERT INTO Seguidos VALUES (?,?)";
       try ( PreparedStatement ps = connection.prepareStatement(query)){
+        //rellenamos con los ids, del usuario que esta en sesion y el usuario al que va a seguir
         ps.setInt(1, id1);
         ps.setInt(2, id2);
+        //Ejecutamos el update
         ps.executeUpdate();
       }
     }
 
+    /*
+    *El usuario con id1 deja de seguir al usuario con id2 y actualiza el campo en la tabla de seguidos
+    */
     public void unfollow(int id1, int id2) throws SQLException{
+      //Creamos el query con el que buscamos la línea con los ids de los interesados y las borramos
       String query = "DELETE FROM Seguidos WHERE user=? AND seguido=?";
       try ( PreparedStatement ps = connection.prepareStatement(query)){
+        //rellenamos con los ids que se nos pasan
         ps.setInt(1, id1);
         ps.setInt(2, id2);
+        //ejecutamos el update
         ps.executeUpdate();
       }
     }
-
+    /*
+    *Comprobamos si el usuario con id1 esta siguiendo o no al usuario con id2
+    *Si le sigue, devolvemos un true
+    *En caso contrario, devolvemos un false
+    */
     public boolean followed(int id1, int id2) throws SQLException{
       boolean result = false;
       String query = "SELECT * FROM Seguidos WHERE user=? AND seguido=?";
