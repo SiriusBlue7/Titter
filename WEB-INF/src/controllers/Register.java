5c8189c55c2cef8015c485fbd1818e2817cbdac1
Fravi-dev <javi14301@gmail.com>
2021-05-15 19:52:04 +0200

    añadimos comentarios al controlador register

--- WEB-INF/src/controllers/Register.java
+++ WEB-INF/src/controllers/Register.java
@@ -1,5 +1,5 @@
 package controllers;
-import twitter.*;//Esto accede a la libreria que hemos creado nosotros en BookShop y podamos acceder a llamarlo
+import twitter.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -20,35 +20,34 @@ import javax.servlet.http.HttpSession;
 public class Register extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response)
             throws IOException, ServletException {
-        // Obtiene el carro de la compra desde la sesión. Lo crea si no existe.
-        HttpSession session = request.getSession();
 
-      				// Obtiene el catálogo de libros desde la base de datos
-              String short_name = request.getParameter("short_name");
-              String long_name = request.getParameter("long_name");
-              String mail = request.getParameter("mail");
-              String password = request.getParameter("password1");
-              String password2 = request.getParameter("password2");
-              try(DBManager db = new DBManager()){
-        				int resultado = db.isDataFree(short_name, long_name,mail);
+        HttpSession session = request.getSession();//iniciamos sesion
+        //Recogemos los parametros que nos ha enviado el usuario
+        String short_name = request.getParameter("short_name");
+        String long_name = request.getParameter("long_name");
+        String mail = request.getParameter("mail");
+        String password = request.getParameter("password1");
+        String password2 = request.getParameter("password2");
+        try(DBManager db = new DBManager()){
+  				int resultado = db.isDataFree(short_name, long_name,mail);
 
-          				if (resultado == 0) {
-                    //creamos el usuario que vamos a guardar en la
-                    User new_user= new User();
-                    new_user.setShort_name(short_name);
-                    new_user.setLong_name(long_name);
-                    new_user.setMail(mail);
-                    new_user.setPassword(password);
+  				if (resultado == 0) {
+            //creamos el usuario que vamos a guardar en la
+            User new_user= new User();
+            new_user.setShort_name(short_name);
+            new_user.setLong_name(long_name);
+            new_user.setMail(mail);
+            new_user.setPassword(password);
 
-                    db.addUser(new_user);//Guardamos el nuevo usuario en la BD
+            db.addUser(new_user);//Guardamos el nuevo usuario en la BD
 
-                    session.setAttribute("user", new_user);//Automaticamente iniciamos sesion en la aplicacion
-                    //una vez comprobado que todo esta bien, redirigimos a la pagina principal del usuario
-                    response.sendRedirect("home");
-                  }
-      			   } catch (SQLException | NamingException e){
-      					e.printStackTrace();
-      					response.sendError(500);
-      			}
+            session.setAttribute("user", new_user);//Automaticamente iniciamos sesion en la aplicacion
+            //una vez comprobado que todo esta bien, redirigimos a la pagina principal del usuario
+            response.sendRedirect("home");
+          }
+		   } catch (SQLException | NamingException e){
+				e.printStackTrace();
+				response.sendError(500);
+			}
     }
 }
